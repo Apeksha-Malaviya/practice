5115149bd55fe14ff09802cbd077e227c8595ebb
Apeksha-Malaviya <apekshamalaviya04@gmail.com>
2023-08-14 10:21:02 +0530

    14-8-2023

---
+++ script.js
@@ -0,0 +1,19 @@
+let namasteBtn = document.querySelector('button');
+namasteBtn. addEventListener('click', inputMsg);
+
+function inputMsg(){
+    let name = prompt( 'Enter Name of student');
+    namasteBtn.textContent='Roll No. 1:'+ name;
+}
+
+
+
+
+
+
+
+
+
+
+
+
